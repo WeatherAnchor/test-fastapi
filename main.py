b9379fb2bb58cd8b282c353225be2e62b70a7a45
Weather <39768933+WeatherAnchor@users.noreply.github.com>
2024-02-28 20:18:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/test")
+async def test():
+    return {"message": "This is a test"}
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
